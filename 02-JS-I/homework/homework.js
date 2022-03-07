b49d767aa81011c15f9a8494da0acabcc50f866b
RobertoCRM <rcrmqmuaslp@hotmail.com>
2022-03-07 14:03:04 -0600

    primer cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "adios";
+const nuevoNum = "bye";
 
 // Crea una variable booleana:
 const nuevoBool = null;
